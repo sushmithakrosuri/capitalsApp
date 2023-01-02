3415e51e4b3824402f9fbf58cf287e89be246947
sushmitha krosri <sushmitha.krosuri11@gmail.com>
2023-01-02 07:18:54 +0000

    capitalsApp

---
+++ src/components/Capitals/index.js
@@ -0,0 +1,77 @@
+import {Component} from 'react'
+import './index.css'
+
+const countryAndCapitalsList = [
+  {
+    id: 'NEW_DELHI',
+    capitalDisplayText: 'New Delhi',
+    country: 'India',
+  },
+  {
+    id: 'LONDON',
+    capitalDisplayText: 'London',
+    country: 'United Kingdom',
+  },
+  {
+    id: 'PARIS',
+    capitalDisplayText: 'Paris',
+    country: 'France',
+  },
+  {
+    id: 'KATHMANDU',
+    capitalDisplayText: 'Kathmandu',
+    country: 'Nepal',
+  },
+  {
+    id: 'HELSINKI',
+    capitalDisplayText: 'Helsinki',
+    country: 'Finland',
+  },
+]
+
+// Write your code here
+
+class Capitals extends Component {
+  state = {activeCapitalId: countryAndCapitalsList[0].id}
+
+  getCapital = event => {
+    this.setState({activeCapitalId: event.target.value})
+  }
+
+  getCountry = () => {
+    const {activeCapitalId} = this.state
+    const activeCountryAndCapital = countryAndCapitalsList.find(
+      eachCapital => eachCapital.id === activeCapitalId,
+    )
+    return activeCountryAndCapital.country
+  }
+
+  render() {
+    const {activeCapitalId} = this.state
+
+    const country = this.getCountry(activeCapitalId)
+    return (
+      <div className="bg-container">
+        <div className="card-container">
+          <h1 className="heading">Countries And Capitals</h1>
+          <div className="input-box">
+            <select
+              onChange={this.getCapital}
+              className="capital"
+              value={activeCapitalId}
+            >
+              {countryAndCapitalsList.map(eachCapital => (
+                <option key={eachCapital.id} value={eachCapital.id}>
+                  {eachCapital.capitalDisplayText}
+                </option>
+              ))}
+            </select>
+            <p className="paragraph">is capital of which country?</p>
+          </div>
+          <p className="answer">{country}</p>
+        </div>
+      </div>
+    )
+  }
+}
+export default Capitals
